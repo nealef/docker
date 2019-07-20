aced82b1a1b17c0a6dda1530c15f5d94362d381c
Vedarth <vedarth.sharma@gmail.com>
2019-07-21 01:10:45 +0530

    Fix the pipeline

--- buildall.sh
+++ buildall.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
-original_path = $(pwd)
+original_path=$(pwd)
 $(declare -a blacklist=(base/ microclimate/ origin/ akka/ earthquake/ grafana/ react/))
+
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
 
 for x in $(ls -d */)
 do
-    if [ $x != 'base/' ]
+    containsElement $x "${blacklist[@]}"
+    if [ !$(echo $?) ]
     then
+        echo $x
         $(cd $x ; make all ; cd $original_path)
     fi
 done
